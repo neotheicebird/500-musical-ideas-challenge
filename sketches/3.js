8c5b06c3011b9895e55ab1fb493c2c95e89007ae
Prashanth <neotheicebird@gmail.com>
2026-04-06 15:47:54 +0530

    #3: modifying #2 to make a ladder of notes and got this cool mario collecting coin or 1UP kind of melody

--- sketches/3.js
+++ sketches/3.js
@@ -1,3 +1,5 @@
+//MARIO like sfx, coin collect fx
+samples("github:eddyflux/crate");
 // inspired by this video: https://youtu.be/6aezSL_GvZA?si=qbSY47jBOPewdoVv
 let royal_chords = chord("<FM7 G7 Em7 Am7>"); // Royal Road Progression in C Major
 let canon_chords = chord("<D A Bm F#m G D G A>"); // canon progression in D
@@ -5,6 +7,9 @@ let canon_chords = chord("<D A Bm F#m G D G A>"); // canon progression in D
 // play 8 cycles of first prog then 8 cycles of 2nd to play each progression twice in seq
 let chords = arrange([8, royal_chords], [8, canon_chords]);
 
-$: n("7 8 [10 9] 8").set(chords).voicing().dec(0.2); // notes voiced to chord based on current chord's base note?
+$: n("7 8 [10 9] 8 [1 2 2 1 2 4 4 2 4 6 6 4 6 8 8 6 8 10 10 8]")
+  .set(chords)
+  .voicing()
+  .dec(0.2)
+  ._punchcard(); // notes voiced to chord based on current chord's base note?
 $: chords.struct("- [x x] - x").voicing().room(0.5).gain(0.5); // chord pattern
-// TODO: The bass root should change to d2 when playing canon chords
